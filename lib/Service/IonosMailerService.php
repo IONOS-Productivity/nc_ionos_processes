3fbf4ad9a4262c97ab93b7c224a2d77c74d558cc
Thomas Lehmann <t.lehmann@strato.de>
2024-10-18 17:43:11 +0200

    chore: stub for the mail service

---
+++ lib/Service/IonosMailerService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace OCA\IonosProcesses\Listener;
+
+/**
+ * Client for the internal mail delivery web service.
+ */
+class IonosMailerService {
+	public function __construct(
+	) {
+	}
+
+	/**
+	 * Send mails to the mail delivery web service
+	 * @param string eventName name of the event
+	 * @param array associative array of variables to the mail service
+	 */
+	public function send(string $eventName, array $variables): void {
+		// Stub
+	}
+}
