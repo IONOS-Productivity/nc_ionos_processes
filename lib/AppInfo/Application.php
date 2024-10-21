74b105901fd16e9c658425dc63ff5db1ae913124
Thomas Lehmann <t.lehmann@strato.de>
2024-10-21 10:11:08 +0200

    chore: apply composer cs:fix changes
    Problem: .editorconfig of Nextcloud wants tabs as indentation, composer
    wants spaces.
    
    Signed-off-by: Thomas Lehmann <t.lehmann@strato.de>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -27,17 +27,21 @@ use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
 
-class Application extends App implements IBootstrap {
-	public const APP_ID = 'nc_ionos_processes';
+class Application extends App implements IBootstrap
+{
+    public const APP_ID = 'nc_ionos_processes';
 
-	/** @psalm-suppress PossiblyUnusedMethod */
-	public function __construct() {
-		parent::__construct(self::APP_ID);
-	}
+    /** @psalm-suppress PossiblyUnusedMethod */
+    public function __construct()
+    {
+        parent::__construct(self::APP_ID);
+    }
 
-	public function register(IRegistrationContext $context): void {
-	}
+    public function register(IRegistrationContext $context): void
+    {
+    }
 
-	public function boot(IBootContext $context): void {
-	}
+    public function boot(IBootContext $context): void
+    {
+    }
 }
